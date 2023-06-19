a240e2f292f0ed09b38c0a5e75c0c985682e4909
chenglu4343 <chenglu4343@qq.com>
2023-06-19 09:00:04 +0800

    ✨ feat: try pnpm patch

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 /* eslint-disable no-console */
 import { add } from '@packages/utils'
+import { join } from 'lodash-es'
 
 console.log('Hello World!')
 
 console.log(add(1, 2))
+
+join(['a', 'b'], '-')
