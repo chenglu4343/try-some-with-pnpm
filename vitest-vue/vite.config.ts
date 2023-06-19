1683203dd46f8fec84d6d66b3f17c3535808ba75
chenglu4343 <chenglu4343@qq.com>
2023-06-19 16:58:54 +0800

    🦄 refactor: merge vite-vue-playground in vitest-vue

--- vitest-vue/vite.config.ts
+++ vitest-vue/vite.config.ts
@@ -1,10 +1,21 @@
 // import { defineConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+
+  resolve: {
+    alias: {
+      '@': './src',
+    },
+  },
+
   test: {
     environment: 'happy-dom',
     setupFiles: ['./vitestSetup/index.ts'],
