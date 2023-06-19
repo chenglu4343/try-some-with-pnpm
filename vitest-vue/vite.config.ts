c852341d34e2203463330abb66aa2ffc18856ff0
chenglu4343 <chenglu4343@qq.com>
2023-06-19 11:53:06 +0800

    🐳 chore: add vitest

--- vitest-vue/vite.config.ts
+++ vitest-vue/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
